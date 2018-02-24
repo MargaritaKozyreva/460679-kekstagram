6ea88ba8593e024176a6ee24a61abc7bd99880a1
Маргарита <margaritakozyreva@mail.ru>
2018-02-24 19:23:28 +0300

    модульность

--- js/filters.js
+++ js/filters.js
@@ -1,38 +1,73 @@
 'use strict';
 (function () {
-
   var uploadForm = document.querySelector('#upload-select-image');
-  var uploadEffect = uploadForm.querySelector('.upload-effect-controls');
   var effectImagePreview = uploadForm.querySelector('.effect-image-preview');
+  var uploadEffect = uploadForm.querySelectorAll('[name="effect"]');
+  var uploadEffectLevel = uploadForm.querySelector('.upload-effect-level');
+  var uploadEffectLevelLine = uploadForm.querySelector('.upload-effect-level-line');
+  var uploadLevelPin = uploadForm.querySelector('.upload-effect-level-pin');
+  var uploadLevelValue = uploadForm.querySelector('.upload-effect-level-value');
+  var uploadLevelVal = uploadForm.querySelector('.upload-effect-level-val');
 
   var filterValue = 'none';
 
-  var changeFilter = function (evt) {
-    var target = evt.target;
-    if (target.type === 'radio') {
-      var filter = evt.target.value;
-      clearFilter(filter);
-    }
-  };
+  uploadEffectLevel.style.display = 'none';
+  for (var i = 0; i < uploadEffect.length; i++) {
+    uploadEffect[i].addEventListener('click', function (evt) {
+      for (var j = 0; j < uploadEffect.length; j++) {
+        effectImagePreview.classList.remove('effect-' + uploadEffect[j].value);
+      }
+      uploadEffectLevel.style.display = 'block';
+      effectImagePreview.classList.add('effect-' + evt.target.value);
+      filterValue = evt.target.value;
+      if (filterValue === 'none') {
+        uploadEffectLevel.style.display = 'none';
+      }
+      uploadLevelPin.style.left = '100%';
+      uploadLevelVal.style.width = '100%';
+      effectImagePreview.style.filter = '';
+    });
+  }
 
-  var clearFilter = function (filter) {
-    effectImagePreview.classList = '';
-    effectImagePreview.style = '';
-    effectImagePreview.style.filter = '';
-    effectImagePreview.classList.add('effect-' + filter);
-    filterValue = filter;
-  };
+  uploadLevelPin.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var startCoords = {
+      x: evt.clientX,
+    };
 
-  uploadEffect.addEventListener('click', changeFilter);
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      var shift = {
+        x: moveEvt.clientX - startCoords.x
+      };
+      startCoords = {
+        x: moveEvt.clientX
+      };
+      var newCoords = uploadLevelPin.offsetLeft + shift.x;
+      var newCoordsMaxWidth = uploadEffectLevelLine.offsetWidth;
+      var newCoordsPercent = newCoordsMaxWidth / 100;
 
-  var setLevelEffect = function (val) {
-    switch (filterValue) {
-      case 'chrome': effectImagePreview.style.filter = 'grayscale(' + val / 100 + ')'; break;
-      case 'sepia': effectImagePreview.style.filter = 'sepia(' + val / 100 + ')'; break;
-      case 'marvin': effectImagePreview.style.filter = 'invert(' + val + '%' + ')'; break;
-      case 'phobos': effectImagePreview.style.filter = 'blur(' + val * 3 / 100 + 'px' + ')'; break;
-      case 'heat': effectImagePreview.style.filter = 'brightness(' + val * 3 / 100 + ')'; break;
-    }
-  };
-  setLevelEffect();
+      if (newCoords >= 0 && newCoords <= newCoordsMaxWidth) {
+        uploadLevelPin.style.left = newCoords + 'px';
+        uploadLevelVal.style.width = newCoords / newCoordsPercent + '%';
+        uploadLevelValue.value = newCoords / newCoordsPercent;
+        switch (filterValue) {
+          case 'chrome': effectImagePreview.style.filter = 'grayscale(' + uploadLevelValue.value / 100 + ')'; break;
+          case 'sepia': effectImagePreview.style.filter = 'sepia(' + uploadLevelValue.value / 100 + ')'; break;
+          case 'marvin': effectImagePreview.style.filter = 'invert(' + uploadLevelValue.value + '%' + ')'; break;
+          case 'phobos': effectImagePreview.style.filter = 'blur(' + uploadLevelValue.value * 3 / 100 + 'px' + ')'; break;
+          case 'heat': effectImagePreview.style.filter = 'brightness(' + uploadLevelValue.value * 3 / 100 + ')'; break;
+        }
+      }
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
 })();
